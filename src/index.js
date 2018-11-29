bc02efaa0eaaa8872587bcc67164ea381562d48e
Mateusz Dominiak <matteuszd@gmail.com>
2018-11-29 18:43:03 +0100

    New components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app";
+import App from "./Components/App";
 import "./styles.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
